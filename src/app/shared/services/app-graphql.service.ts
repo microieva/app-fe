08a30451e679465dca4811e8491eace73bf3fc1b
microieva <ieva.vyliaudaite@me.com>
2024-09-24 10:18:38 +0300

    cleanup

--- src/app/shared/services/app-graphql.service.ts
+++ src/app/shared/services/app-graphql.service.ts
@@ -1,35 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Apollo, gql } from 'apollo-angular';
-import { Observable, firstValueFrom } from 'rxjs';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
 })
 export class AppGraphQLService {
-    
-    /*constructor(
-        private apollo: Apollo
-    ) {}
-
-    send(query: string, variables?: Object): Observable<any> {
-        return this.apollo
-            .watchQuery(
-                {
-                    query: gql`${query}`,
-                    variables,
-                    fetchPolicy: 'network-only',
-                    pollInterval: 3000
-                }).valueChanges
-    }
-
-    mutate(mutation: string, variables: Object): Observable<any> {
-        return this.apollo
-            .mutate({
-                mutation: gql`${mutation}`,
-                variables,
-                fetchPolicy: 'network-only'
-            })
-    }*/
     constructor(private apollo: Apollo) {}
 
     async send(query: string, variables?: Object): Promise<any> {
