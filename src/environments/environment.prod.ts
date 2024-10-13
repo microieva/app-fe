3f9f9bdefe34355c185f0cbf01a2f34801ab85d8
Ieva Vyliaudaite <ievavyliaudaite@Ieva-MacBook-Pro.local>
2024-10-13 15:07:00 +0300

    add port 80 to socketUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   url: 'https://health-center-ha99.onrender.com/graphql' ,
   triggerTime: '00:05:00',
-  socketUrl: 'https://health-center-ha99.onrender.com',
+  socketUrl: 'https://health-center-ha99.onrender.com:80',
   adminId: 4
 };
