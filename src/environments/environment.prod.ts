d3769d62516f9258f187df1ea6fecc6802dad71c
Ieva Vyliaudaite <ievavyliaudaite@Ieva-MacBook-Pro.local>
2024-10-13 13:54:41 +0300

    adminId for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   url: 'https://health-center-ha99.onrender.com/graphql' ,
   triggerTime: '00:05:00',
   socketUrl: 'https://health-center-ha99.onrender.com:4001',
-  adminId: 8
+  adminId: 4
 };
