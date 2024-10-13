6d60ed76124580e6f467541f503478d58abe3eaa
Ieva Vyliaudaite <ievavyliaudaite@Ieva-MacBook-Pro.local>
2024-10-13 15:59:01 +0300

    https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   url: 'https://health-center-ha99.onrender.com/graphql' ,
   triggerTime: '00:05:00',
-  socketUrl: 'wws://health-center-ha99.onrender.com',
+  socketUrl: 'https://health-center-ha99.onrender.com',
   adminId: 4
 };
