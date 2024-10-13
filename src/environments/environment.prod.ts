2856c76362b8237d5d2a30badb7840720e4fd679
Ieva Vyliaudaite <ievavyliaudaite@Ieva-MacBook-Pro.local>
2024-10-13 15:27:11 +0300

    remove port 80

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   url: 'https://health-center-ha99.onrender.com/graphql' ,
   triggerTime: '00:05:00',
-  socketUrl: 'https://health-center-ha99.onrender.com:80',
+  socketUrl: 'https://health-center-ha99.onrender.com',
   adminId: 4
 };
