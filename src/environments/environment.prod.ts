620b7de137ce5a96e869f04b72e4c37810bda167
Ieva Vyliaudaite <ievavyliaudaite@Ieva-MacBook-Pro.local>
2024-10-16 12:48:28 +0300

    env prod add port

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   url: 'https://health-center-ha99.onrender.com/graphql' ,
   triggerTime: '00:05:00',
-  socketUrl: 'wss://health-center-ha99.onrender.com',
+  socketUrl: 'wss://health-center-ha99.onrender.com:4000',
   adminId: 4
 };
