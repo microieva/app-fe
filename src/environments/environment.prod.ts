26d1a17523a34cc5a5c00189221e565b83455ab7
Ieva Vyliaudaite <ievavyliaudaite@Ieva-MacBook-Pro.local>
2024-10-16 15:32:40 +0300

    remove port

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   url: 'https://health-center-ha99.onrender.com/graphql' ,
   triggerTime: '00:05:00',
-  socketUrl: 'wss://health-center-ha99.onrender.com:4000',
+  socketUrl: 'wss://health-center-ha99.onrender.com',
   adminId: 4
 };
