f10c039190b5322ccaf5aecbc63514d649567454
Ieva Vyliaudaite <ievavyliaudaite@Ieva-MacBook-Pro.local>
2025-02-09 13:03:08 +0200

    reverse vars

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,14 @@
 export const environment = {
   production: true,
-  url:'',
-  tokenEndpoint:'',
-  clientId:'',
-  clientSecret:'',
-  redirectUri:'',
-  triggerTime:'',
-  adminId:'',
-  authEndpoint:'',
-  client_id:'',
-  socketUrl:'',
-  httpTriggerAiUrl:''
+  url: 'https://health-center.happypond-6e715cde.northeurope.azurecontainerapps.io/graphql' ,
+  triggerTime: '00:05:00',
+  socketUrl: 'wss://health-center.happypond-6e715cde.northeurope.azurecontainerapps.io',
+  adminId: 8,
+  tokenEndpoint: 'https://health-center.sandbox.signicat.com/auth/open/connect/token',
+  authEndpoint: 'https://health-center.sandbox.signicat.com/auth/open/connect/authorize',
+  clientId: 'sandbox-itchy-wheel-954',
+  clientSecret: 'EJTOPAOXSS2c8bPpMOeJpTe64DvbFdWBS2wH5ytbvT7Tt5Yh',
+  redirectUri: 'https://wonderful-dune-0e4733c03.5.azurestaticapps.net',
+  client_id: '234359946846-qk29f6skfdjaaoklv41g49f1du23c177.apps.googleusercontent.com',
+  httpTriggerAiUrl:'https://healthcenterfunctionapp.azurewebsites.net/api/httpTriggerAi?'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,14 @@
 export const environment = {
     production: false,
-    url:'',
-    tokenEndpoint:'',
-    clientId:'',
-    clientSecret:'',
-    redirectUri:'',
-    triggerTime:'',
-    adminId:'',
-    authEndpoint:'',
-    client_id:'',
-    socketUrl:'',
-    httpTriggerAiUrl:''
+    url: 'http://localhost:4000/graphql',
+    triggerTime: '00:05:00',
+    socketUrl: 'http://localhost:4000',
+    adminId: 8,
+    tokenEndpoint: 'https://health-center.sandbox.signicat.com/auth/open/connect/token',
+    authEndpoint: 'https://health-center.sandbox.signicat.com/auth/open/connect/authorize', 
+    clientId: 'sandbox-itchy-wheel-954',
+    clientSecret: 'EJTOPAOXSS2c8bPpMOeJpTe64DvbFdWBS2wH5ytbvT7Tt5Yh',
+    redirectUri: 'http://localhost:4200',
+    client_id: '234359946846-qk29f6skfdjaaoklv41g49f1du23c177.apps.googleusercontent.com',
+    httpTriggerAiUrl:'http://localhost:7071/api/httpTriggerAi'
   };
